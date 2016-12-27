1e0c6ab6cfb2c8ec621ce76abffa03ee420bf0ea
Shuhei Koike <koike@slp.nitech.ac.jp>
2016-12-27 19:10:22 +0900

    changed file

--- proverbs.c
+++ proverbs.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
   printf("To be punctual is one of our social duties.\n");
   printf("Stay hungry, stay foolish.\n");
   printf("There is more to life than increasing its speed.\n");
+  printf("A goal without a plan is just a wish..\n");
+
 
   return 0;
 }
