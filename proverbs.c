8bf3848634ef580db71c7c6acfd60f13ef7fb6e8
tmihara-nitech <28414103@stn.nitech.ac.jp>
2016-12-27 14:46:18 +0900

    Update proverbs.c
    add new proverbs

--- proverbs.c
+++ proverbs.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
   printf("To be punctual is one of our social duties.\n");
   printf("Stay hungry, stay foolish.\n");
   printf("There is more to life than increasing its speed.\n");
+  printf("When in Rome, do as the Romans do.\n");
+
 
   return 0;
 }
