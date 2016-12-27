9d2787e24f2b8bfcc3b2505b69507cf27d35f57e
s-shirakawa <28414051@stn.nitech.ac.jp>
2016-12-27 18:55:12 +0900

    add 1 sentence

--- proverbs.c
+++ proverbs.c
@@ -5,6 +5,6 @@ int main(int argc, char *argv[]) {
   printf("To be punctual is one of our social duties.\n");
   printf("Stay hungry, stay foolish.\n");
   printf("There is more to life than increasing its speed.\n");
-
+  printf("Ask, and it will be given to you.\n");
   return 0;
 }
