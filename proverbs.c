4b3df1bb2c064a483ab94a49a6929bc4577771fc
emit-g <wemit.mgtpezy.zotfsent@gmail.com>
2016-12-27 16:58:03 +0900

    my exercise

--- proverbs.c
+++ proverbs.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
   printf("To be punctual is one of our social duties.\n");
   printf("Stay hungry, stay foolish.\n");
   printf("There is more to life than increasing its speed.\n");
-
+  printf("Rome was not built in a day.\n");
+  
   return 0;
 }
